76012745dc1d7847c96ae927d23d98880278cf45
Daniel Vasquez <andresvp2002@gmail.com>
2020-07-07 11:26:24 -0500

    rock paper scissors game with php and basic login

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Daniel Vásquez - Broken Rock Paper Scissors</title>
+<?php require "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container ">
+    <div class="row">
+    <div class="row row-content">
+        <div class="col-xs-5">
+            <h1>Welcome To  Rock Paper Scissors</h1>
+        </div>
+ 
+        </div>
+    </div>
+<div class="row">
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+</p>
+</div>
+</div>
+</body>
+
